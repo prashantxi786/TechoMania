00ef71e8f78971c44fe4bf33e60bb2ab6a9415e3
sudipa <sudipahazra.haldia@gmail.com>
2023-01-20 10:37:28 +0530

    productpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Signup from './Pages/Signup';
+import AllRoutes from './Routes/AllRoutes';
 
 function App() {
   return (
     <div className='App'>
-      <Signup />
+      {/* <Signup /> */}
+      <AllRoutes/>
     </div>
   );
 }
--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import ProductPage from '../Components/Authentication/ProductPage';
+import SingleProduct from '../Components/Authentication/SingleProductPage';
+
+
+
 
 const AllRoutes = () => {
-  return <Routes></Routes>;
+  return <Routes>
+    <Route path="/ProductPage" element={<ProductPage/>}/>
+    <Route path="/ProductPage/:id" element={<SingleProduct/>}/>
+  </Routes>;
 };
 
 export default AllRoutes;
