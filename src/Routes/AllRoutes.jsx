7a752208a844ce22e660cc9d9cbc8c4e3e1631e4
TheLegendaryWarrior <sanket.potdar563@gmail.com>
2023-01-21 19:20:23 +0530

    Added some functionalities

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import PrivateRoute from '../Components/Authentication/PrivateRoute';
 import Homepage from '../Pages/Homepage';
+import Cart from '../Pages/Cart';
 import Login from '../Pages/Login';
 import Signup from '../Pages/Signup';
-import ProductPage from '../Components/Authentication/ProductPage';
-import SingleProduct from '../Components/Authentication/SingleProductPage';
+import ProductPage from '../Pages/ProductPage';
+import SingleProduct from '../Pages/SingleProductPage';
 const AllRoutes = () => {
   return (
     <Routes>
@@ -30,6 +30,8 @@ const AllRoutes = () => {
       <Route path='/ProductPage/:id' element={<SingleProduct />} />
       {/* <Route path="/admin" element={<AdminPanel/>}/> */}
       {/* <Route path="/admin/products" element={<Products/>}/> */}
+      {/* <Route path="/" element={<Homepage/>} /> */}
+      <Route path='/cart' element={<Cart />} />
     </Routes>
   );
 };
